43b9c0cb906daad43d54eecbc2c1d3a8d7c2ff5e
RusCriminal <143919047+RusCriminal@users.noreply.github.com>
2025-02-22 21:20:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,13 @@ from world import World
 
 app = Ursina()
 
-# Создание мира
 world = World()
-
-# Создание игрока
 player = Player()
 
-# Настройка освещения
-DirectionalLight(parent=player, direction=Vec3(1, -1, 1), color=color.white)
+# Включение отладки коллизий
+window.collider_visible = True
 
 def update():
-    pass  # Общая логика игры
-
-def input(key):
-    if key == 'escape':
-        application.quit()
+    pass
 
 app.run()
