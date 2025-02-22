b2a47f5f8455f65d9f60d43fc5db293fe1246504
RusCriminal <143919047+RusCriminal@users.noreply.github.com>
2025-02-22 21:49:09 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-class Settings:
-    # ... предыдущие настройки
-    CAMERA_DISTANCE = 4  # Базовое расстояние
-    CAMERA_HEIGHT = 2    # Высота камеры
-    MIN_CAMERA_DIST = 2  # Минимальное расстояние
-    MAX_CAMERA_DIST = 8  # Максимальное расстояние
